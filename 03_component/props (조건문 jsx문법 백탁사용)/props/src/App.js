39768c01f15e1dbc4ce19b1628f561d098bd4c9f
JeongHyein <jhi2359@naver.com>
2023-01-25 16:33:19 +0900

    props study
    props 이해, jsx 문법 공부, ` 사용해서 jsx 내부에 조건문 만들기

---
+++ 03_component/props (조건문 jsx문법 백탁사용)/props/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <div>
+    <MyComponent name='아무개' age='10'/>
+    <MyComponent name='react'/>
+    </div>
+  );
+}
+
+export default App;
+
