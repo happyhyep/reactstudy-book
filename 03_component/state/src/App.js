b81d0ef8f5532839504ae346a32421da007bd2c6
JeongHyein <jhi2359@naver.com>
2023-02-03 16:52:21 +0900

    state

---
+++ 03_component/state/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <MyComponent name='정혜인' age='3'></MyComponent>
+  )
+}
+
+export default App;
